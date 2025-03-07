203ca810b3726061145fa27b23b25ae14c48d846
Brandon Guigo <brandon.guigo@gmail.com>
2025-03-07 19:08:35 +0100

    feat: update TaskEntity model to make EndDate field optional

--- api/models/task_entity.go
+++ api/models/task_entity.go
@@ -3,13 +3,13 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type TaskEntity struct {
-	ID		  string `json:"id" bson:"_id"`
-	Title    string `json:"title" bson:"title" binding:"required"`
-	User primitive.ObjectID `json:"user" bson:"user"`
-	Description *string `json:"description" bson:"description"`
-	StartDate *primitive.DateTime `json:"startDate" bson:"start_date" binding:"required"`
-	EndDate *primitive.DateTime `json:"endDate" bson:"end_date" binding:"required"`
-	Completed *bool `json:"completed" bson:"completed"`
-	CreatedAt string `json:"createdAt" bson:"created_at"`
-	UpdatedAt string `json:"updatedAt" bson:"updated_at"`
+	ID          string              `json:"id" bson:"_id"`
+	Title       string              `json:"title" bson:"title" binding:"required"`
+	User        primitive.ObjectID  `json:"user" bson:"user"`
+	Description *string             `json:"description" bson:"description"`
+	StartDate   *primitive.DateTime `json:"startDate" bson:"start_date"`
+	EndDate     *primitive.DateTime `json:"endDate,omitempty" bson:"end_date"`
+	Completed   *bool               `json:"completed" bson:"completed"`
+	CreatedAt   string              `json:"createdAt" bson:"created_at"`
+	UpdatedAt   string              `json:"updatedAt" bson:"updated_at"`
 }
