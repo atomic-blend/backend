4cb56c668f3b5a6a074f39f17652cd1594a49ea8
Brandon Guigo <brandon.guigo@gmail.com>
2025-07-31 15:54:52 +0200

    feat: configure amqp producer in mail server
    Signed-off-by: Brandon Guigo <brandon.guigo@gmail.com>

--- mail-server/main.go
+++ mail-server/main.go
@@ -1,81 +1,16 @@
 package main
 
 import (
-	"errors"
-	"io"
 	"os"
 	"time"
 
-	"github.com/emersion/go-sasl"
+	smtpserver "github.com/atomic-blend/backend/mail-server/smtp-server"
+	"github.com/atomic-blend/backend/mail-server/utils/amqp"
 	"github.com/emersion/go-smtp"
 	"github.com/joho/godotenv"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
-
-// The Backend implements SMTP server methods.
-type Backend struct{}
-
-// NewSession is called after client greeting (EHLO, HELO).
-func (bkd *Backend) NewSession(c *smtp.Conn) (smtp.Session, error) {
-	return &Session{}, nil
-}
-
-// A Session is returned after successful login.
-type Session struct {
-	auth bool
-}
-
-// AuthMechanisms returns a slice of available auth mechanisms; only PLAIN is
-// supported in this example.
-func (s *Session) AuthMechanisms() []string {
-	return []string{sasl.Plain}
-}
-
-// Auth is the handler for supported authenticators.
-func (s *Session) Auth(mech string) (sasl.Server, error) {
-	return sasl.NewPlainServer(func(identity, username, password string) error {
-		if username != "username" || password != "password" {
-			return errors.New("invalid_credentials")
-		}
-		s.auth = true
-		return nil
-	}), nil
-}
-
-func (s *Session) Mail(from string, opts *smtp.MailOptions) error {
-	if !s.auth {
-		return smtp.ErrAuthRequired
-	}
-	log.Info().Msgf("Mail from: %s", from)
-	return nil
-}
-
-func (s *Session) Rcpt(to string, opts *smtp.RcptOptions) error {
-	if !s.auth {
-		return smtp.ErrAuthRequired
-	}
-	log.Info().Msgf("Rcpt to: %s", to)
-	return nil
-}
-
-func (s *Session) Data(r io.Reader) error {
-	if !s.auth {
-		return smtp.ErrAuthRequired
-	}
-	if b, err := io.ReadAll(r); err != nil {
-		return err
-	} else {
-		log.Info().Msgf("Data: %s", string(b))
-	}
-	return nil
-}
-
-func (s *Session) Reset() {}
-
-func (s *Session) Logout() error {
-	return nil
-}
 
 func main() {
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
@@ -94,8 +29,12 @@ func main() {
 		host = "0.0.0.0"
 	}
 
-	be := &Backend{}
+	amqp.InitProducerAmqp()
 
+	// instanciate the smtp backend
+	be := &smtpserver.Backend{}
+
+	// create the smtp server
 	s := smtp.NewServer(be)
 
 	s.Addr = host + ":" + port
