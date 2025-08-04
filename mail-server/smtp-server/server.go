9b6bbd135703ec5f567543f0f39ce2091e484933
Brandon Guigo <brandon.guigo@gmail.com>
2025-08-04 10:51:36 +0200

    feat: make the smtp server catch all the infos of the sender before sending amqp
    Signed-off-by: Brandon Guigo <brandon.guigo@gmail.com>

--- mail-server/smtp-server/server.go
+++ mail-server/smtp-server/server.go
@@ -2,8 +2,11 @@ package smtpserver
 
 import (
 	"bytes"
+	"crypto/rand"
+	"encoding/hex"
 	"errors"
 	"io"
+	"net"
 
 	"github.com/atomic-blend/backend/mail-server/utils/amqp"
 	"github.com/emersion/go-sasl"
@@ -16,12 +19,35 @@ type Backend struct{}
 
 // NewSession is called after client greeting (EHLO, HELO).
 func (bkd *Backend) NewSession(c *smtp.Conn) (smtp.Session, error) {
-	return &Session{}, nil
+	// Get client IP address
+	var clientIP string
+	if addr, ok := c.Conn().RemoteAddr().(*net.TCPAddr); ok {
+		clientIP = addr.IP.String()
+	}
+
+	// Get HELO / hostname from connection
+	hostname := c.Hostname()
+
+	// Generate a unique queue ID
+	queueID := generateQueueID()
+
+	return &Session{
+		clientIP: clientIP,
+		hostname: hostname,
+		queueID:  queueID,
+	}, nil
 }
 
 // A Session is returned after successful login.
 type Session struct {
-	auth bool
+	auth     bool
+	clientIP string
+	// HELO / hostname
+	hostname string
+	queueID  string
+	from     string
+	rcpts    []string
+	user     string
 }
 
 // AuthMechanisms returns a slice of available auth mechanisms; only PLAIN is
@@ -37,6 +63,7 @@ func (s *Session) Auth(mech string) (sasl.Server, error) {
 			return errors.New("invalid_credentials")
 		}
 		s.auth = true
+		s.user = username
 		return nil
 	}), nil
 }
@@ -45,6 +72,7 @@ func (s *Session) Mail(from string, opts *smtp.MailOptions) error {
 	if !s.auth {
 		return smtp.ErrAuthRequired
 	}
+	s.from = from
 	log.Info().Msgf("Mail from: %s", from)
 	return nil
 }
@@ -53,6 +81,7 @@ func (s *Session) Rcpt(to string, opts *smtp.RcptOptions) error {
 	if !s.auth {
 		return smtp.ErrAuthRequired
 	}
+	s.rcpts = append(s.rcpts, to)
 	log.Info().Msgf("Rcpt to: %s", to)
 	return nil
 }
@@ -63,15 +92,36 @@ func (s *Session) Data(r io.Reader) error {
 		return err
 	}
 
-	amqp.PublishMessage("mail", "received", map[string]interface{}{
-		"content": buf.String(),
-	})
+	// Prepare message data for Rspamd analysis
+	messageData := map[string]interface{}{
+		"content":    buf.String(),
+		"ip":         s.clientIP,
+		"hostname":   s.hostname,
+		"from":       s.from,
+		"rcpt":       s.rcpts,
+		"queue_id":   s.queueID,
+		"user":       s.user,
+		"deliver_to": s.rcpts[0], // Use first recipient as deliver_to
+	}
+
+	amqp.PublishMessage("mail", "received", messageData)
 
 	return nil
 }
 
-func (s *Session) Reset() {}
+func (s *Session) Reset() {
+	// Reset session data for new message
+	s.from = ""
+	s.rcpts = nil
+}
 
 func (s *Session) Logout() error {
 	return nil
+}
+
+// generateQueueID creates a unique queue ID for message tracking
+func generateQueueID() string {
+	bytes := make([]byte, 8)
+	rand.Read(bytes)
+	return hex.EncodeToString(bytes)
 }
