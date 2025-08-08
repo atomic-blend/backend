ce1f7b21284646747573ac0069cd1e7877e9bb80
Brandon Guigo <brandon.guigo@gmail.com>
2025-08-08 10:50:53 +0200

    feat: store all the headers of the mail encrypted in DB
    Signed-off-by: Brandon Guigo <brandon.guigo@gmail.com>

--- mail/models/mail.go
+++ mail/models/mail.go
@@ -4,17 +4,6 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-// MailHeaders represents the email headers
-type MailHeaders struct {
-	From      string `bson:"from" json:"from"`
-	To        string `bson:"to" json:"to"`
-	Subject   string `bson:"subject" json:"subject"`
-	Date      string `bson:"date" json:"date"`
-	MessageID string `bson:"message_id" json:"message_id"`
-	Cc        string `bson:"cc" json:"cc"`
-	Bcc       string `bson:"bcc" json:"bcc"`
-}
-
 // MailAttachment represents a file attachment
 type MailAttachment struct {
 	Filename    string `bson:"filename" json:"filename"`
@@ -28,7 +17,7 @@ type MailAttachment struct {
 type Mail struct {
 	ID             primitive.ObjectID  `bson:"_id" json:"id"`
 	UserID         primitive.ObjectID  `bson:"user_id" json:"user_id"`
-	Headers        MailHeaders         `bson:"headers" json:"headers"`
+	Headers        interface{}         `bson:"headers" json:"headers"`
 	TextContent    string              `bson:"text_content" json:"text_content"`
 	HTMLContent    string              `bson:"html_content" json:"html_content"`
 	Attachments    []MailAttachment    `bson:"attachments" json:"attachments"`
