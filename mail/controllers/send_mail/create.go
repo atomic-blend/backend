d0a3b4eb45811d9bc5c2664b41f54ebbc3e29961
Brandon Guigo <brandon.guigo@gmail.com>
2025-08-09 12:14:58 +0200

    fix: publishing error for message (routing key)
    Signed-off-by: Brandon Guigo <brandon.guigo@gmail.com>

--- mail/controllers/send_mail/create.go
+++ mail/controllers/send_mail/create.go
@@ -3,6 +3,7 @@ package send_mail
 import (
 	"context"
 	"net/http"
+	"os"
 
 	"connectrpc.com/connect"
 	userv1 "github.com/atomic-blend/backend/grpc/gen/user/v1"
@@ -10,6 +11,9 @@ import (
 	"github.com/atomic-blend/backend/mail/grpc/clients"
 	"github.com/atomic-blend/backend/mail/models"
 	"github.com/atomic-blend/backend/mail/utils/amqp"
+	"github.com/atomic-blend/backend/mail/utils/s3"
+	awss3 "github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/google/uuid"
 	"github.com/rs/zerolog/log"
 
 	"github.com/gin-gonic/gin"
@@ -79,6 +83,12 @@ func (c *Controller) CreateSendMail(ctx *gin.Context) {
 		return
 	}
 
+	s3Service, err := s3.NewS3Service(os.Getenv("AWS_BUCKET"))
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to create S3 service")
+		return
+	}
+
 	// Create send mail entity
 	sendMail := &models.SendMail{
 		Mail:       encryptedMail.ToMailEntity(),
@@ -86,10 +96,36 @@ func (c *Controller) CreateSendMail(ctx *gin.Context) {
 		Trashed:    false,
 	}
 
-	// TODO: setup transactional upload for mail with rollback on failure (s3 and mongo)
+	// Generate encrypted attachments upload requests to send them to S3
+	encryptedAttachments := make([]*awss3.PutObjectInput, 0)
+	for _, attachment := range encryptedMail.Attachments {
+		uniqueFileID := uuid.New().String()
+		payload, err := s3Service.GenerateUploadPayload(context.Background(), attachment.Data, "send_mail/attachments/"+userPublicKey.Msg.UserId, uniqueFileID, map[string]string{})
+		if err != nil {
+			log.Error().Err(err).Msg("Failed to upload attachment to S3")
+		}
+		sendMail.Mail.Attachments = append(sendMail.Mail.Attachments, models.MailAttachment{
+			StoragePath: *payload.Key,
+			Filename:    attachment.Filename,
+			ContentType: attachment.ContentType,
+			StorageType: "s3",
+			Size:        int64(len(attachment.Data)),
+		})
+		encryptedAttachments = append(encryptedAttachments, payload)
+	}
+
+	// upload the attachments to s3 in bulk
+	uploadedKeys, err := s3Service.BulkUploadFiles(context.Background(), encryptedAttachments)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to upload attachments to S3")
+		return
+	}
+
+	// TODO: [DONE] setup transactional upload for mail with rollback on failure (s3 and mongo)
 	createdSendMail, err := c.sendMailRepo.Create(ctx, sendMail)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		s3Service.BulkDeleteFiles(context.Background(), uploadedKeys)
 		return
 	}
 
@@ -97,8 +133,13 @@ func (c *Controller) CreateSendMail(ctx *gin.Context) {
 	sendMail.CreatedAt = createdSendMail.CreatedAt
 	sendMail.UpdatedAt = createdSendMail.UpdatedAt
 
+	// TODO: fix headers null in db mail
+	//TODO: fix raw mail in payload have all the fields not defined
+
+	log.Debug().Interface("send_mail", rawMail).Msg("Publishing send mail message to queue")
+
 	//TODO: [DONE] publish to message queue
-	amqp.PublishMessage("mail", "mail:sent", map[string]interface{}{
+	amqp.PublishMessage("mail", "sent", map[string]interface{}{
 		"send_mail_id": sendMail.ID.Hex(),
 		"content":      rawMail, // Use the raw mail content for processing
 	})
