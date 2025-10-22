8cd1615e2641f0bbe2a1427998d5249085276447
Brandon Guigo <brandon.guigo@gmail.com>
2025-10-22 10:14:39 +0200

    fix: ip address as public address
    Signed-off-by: Brandon Guigo <brandon.guigo@gmail.com>

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -9,10 +9,21 @@ if [ -z "$PUBLIC_ADDRESS" ]; then
 fi
 
 # Generate the rest API URL based on HTTPS setting
-if [ "$HTTPS" = "true" ]; then
-    REST_API_URL="https://api.${PUBLIC_ADDRESS}"
+# Check if PUBLIC_ADDRESS is an IP address (IPv4 pattern)
+if [[ "$PUBLIC_ADDRESS" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    # It's an IP address, don't add api. prefix
+    if [ "$HTTPS" = "true" ]; then
+        REST_API_URL="https://${PUBLIC_ADDRESS}"
+    else
+        REST_API_URL="http://${PUBLIC_ADDRESS}"
+    fi
 else
-    REST_API_URL="http://api.${PUBLIC_ADDRESS}"
+    # It's a domain name, add api. prefix
+    if [ "$HTTPS" = "true" ]; then
+        REST_API_URL="https://api.${PUBLIC_ADDRESS}"
+    else
+        REST_API_URL="http://api.${PUBLIC_ADDRESS}"
+    fi
 fi
 
 echo "Generated REST API URL: $REST_API_URL"
