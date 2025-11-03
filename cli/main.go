2f468836996d2feefab76183ee0d4906d990c055
Brandon Guigo <brandon.guigo@gmail.com>
2025-11-03 11:34:49 +0100

    feat: add debug logging by env
    Signed-off-by: Brandon Guigo <brandon.guigo@gmail.com>

--- cli/main.go
+++ cli/main.go
@@ -3,8 +3,24 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import rootcmd "github.com/atomic-blend/backend/cli/root_cmd"
+import (
+	"os"
+
+	rootcmd "github.com/atomic-blend/backend/cli/root_cmd"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
 
 func main() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
+	debug := os.Getenv("LOG_LEVEL")
+
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	if debug == "debug" {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
+
 	rootcmd.Execute()
 }
--- cli/self-host/init_cmd/root.go
+++ cli/self-host/init_cmd/root.go
@@ -14,10 +14,15 @@ func NewCommand() *cobra.Command {
 		Long: `The init command sets up a new self-hosted atomic blend instance.
 It guides you through the necessary steps to configure and deploy your instance.`,
 		Run: func(cmd *cobra.Command, args []string) {
-			// Implementation of the init command goes here
-			log.Debug().Str("directory", directory).Msg("Initializing self-hosted atomic blend instance")
+			initSelfHost(cmd, args)
 		},
 	}
 	cmd.Flags().StringVarP(&directory, "directory", "d", "", "Specify the directory to initialize the instance in")
 	return cmd
+}
+
+func initSelfHost(cmd *cobra.Command, args []string) {
+	log.Info().Str("directory", directory).Msg("Initializing self-hosted atomic blend instance")
+
+	log.Info().Msg("Self-hosted atomic blend instance initialized successfully")
 }
